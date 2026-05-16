e4bef7a29734bedd0dee49d0af0bca59d1f8046e
Martin Schneider <mart.schneider@gmail.com>
2026-05-16 15:38:41 -0700

    Remove dead code in GlobalIdentifierMap.

--- src/main/java/io/github/martinschneider/orzo/codegen/identifier/GlobalIdentifierMap.java
+++ src/main/java/io/github/martinschneider/orzo/codegen/identifier/GlobalIdentifierMap.java
@@ -1,13 +1,8 @@
 package io.github.martinschneider.orzo.codegen.identifier;
 
-import java.util.Map;
-
 public class GlobalIdentifierMap {
   // fqn of the class
   public String key;
 
-  public Map<String, IdentifierInfo> values;
-
-  // TODO: this is temporary, merge this into values
   public IdentifierMap variables = new IdentifierMap();
 }
